415e7457d8edd1e313bad100f44c4475102e8b06
KM5075 <koji19991999@gmail.com>
2025-01-01 17:40:47 +0900

    Add Jest

---
+++ studychrono_ts.client/src/Temp.test.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Temp from "./Temp";
+
+/*
+ * サンプルテスト
+ */
+describe("サンプルテスト", () => {
+    test("[正常系]サンプルテスト", async () => {
+        // 実行
+        render(<Temp />);
+
+        // 検証
+        expect(screen.getByText("Hello world!")).toBeInTheDocument();
+    });
+});
