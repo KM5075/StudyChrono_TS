cc455aa97cea3f2ab086374723fc06f72edefd68
KM5075 <koji19991999@gmail.com>
2025-01-03 21:39:37 +0900

    Add : StudyRecord model

---
+++ StudyChrono_TS.Server/Models/StudyRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace StudyChrono_TS.Server.Models;
+
+public class StudyRecord
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int StudyTime { get; set; }
+}
