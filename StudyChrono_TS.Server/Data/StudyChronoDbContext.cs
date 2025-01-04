2fb300b7c5cf7a9a01ccfdd84e29bfbb1efcdc1e
KM5075 <koji19991999@gmail.com>
2025-01-04 16:10:44 +0900

    Add : Add connection to Azrue SQL Database

---
+++ StudyChrono_TS.Server/Data/StudyChronoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyChrono_TS.Server.Models;
+
+namespace StudyChrono_TS.Server.Data;
+
+public class StudyChronoDbContext : DbContext
+{
+    public StudyChronoDbContext(DbContextOptions<StudyChronoDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<StudyRecord> StudyRecords { get; set; }
+}
--- StudyChrono_TS.Server/Program.cs
+++ StudyChrono_TS.Server/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using StudyChrono_TS.Server.Data;
 using StudyChrono_TS.Server.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
+
+builder.Services.AddDbContext<StudyChronoDbContext>(options =>
+    options.UseSqlServer(connection));
+
 
 builder.Services.AddScoped<IRepositories,Repository>();
 
